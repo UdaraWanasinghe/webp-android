315dcbfbd0a4067fdc6e1d47ba35ad1f124bf1f2
UdaraWanasinghe <udara.developer@gmail.com>
2021-11-02 21:19:39 +0530

    cmake configure for libwebp

---
+++ webp-codec/src/main/cpp/encoder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by udara on 11/2/21.
+//
+
+#include "encoder.h"
---
+++ webp-codec/src/main/cpp/include/encoder.h
@@ -0,0 +1,8 @@
+//
+// Created by udara on 11/2/21.
+//
+
+#ifndef WEBP_ANDROID_ENCODER_H
+#define WEBP_ANDROID_ENCODER_H
+
+#endif //WEBP_ANDROID_ENCODER_H
