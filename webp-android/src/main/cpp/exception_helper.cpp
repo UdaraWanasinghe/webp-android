d6bfa8ce34f67cdeba54907ae7607fd1a2438803
Udara Wanasinghe <udara.developer@gmail.com>
2023-06-13 17:02:44 +0530

    bug fixes: clear java exceptions in native code, refactor

--- webp-android/src/main/cpp/exception_helper.cpp
+++ webp-android/src/main/cpp/exception_helper.cpp
@@ -7,13 +7,13 @@
 #include "include/string_formatter.h"
 #include "include/exception_helper.h"
 
-void throwException(JNIEnv *env, jclass *exceptionClass, const char *format, ...) {
+void throwException(JNIEnv *env, jclass *exception_class, const char *format, ...) {
     va_list args;
     va_start(args, format);
 
     std::string message = formatString(format, args);
-    env->ThrowNew(*exceptionClass, message.c_str());
-    env->DeleteLocalRef(*exceptionClass);
+    env->ThrowNew(*exception_class, message.c_str());
+    env->DeleteLocalRef(*exception_class);
 
     va_end(args);
 }
@@ -21,88 +21,88 @@ void throwException(JNIEnv *env, jclass *exceptionClass, const char *format, ...
 void throwNullPointerException(JNIEnv *env, const char *format, ...) {
     va_list args;
     va_start(args, format);
-    jclass exceptionClass = env->FindClass("java/lang/NullPointerException");
-    throwException(env, &exceptionClass, format, args);
+    jclass exception_class = env->FindClass("java/lang/NullPointerException");
+    throwException(env, &exception_class, format, args);
     va_end(args);
 }
 
 void throwIllegalArgumentException(JNIEnv *env, const char *format, ...) {
     va_list args;
     va_start(args, format);
-    jclass exceptionClass = env->FindClass("java/lang/IllegalArgumentException");
-    throwException(env, &exceptionClass, format, args);
+    jclass exception_class = env->FindClass("java/lang/IllegalArgumentException");
+    throwException(env, &exception_class, format, args);
     va_end(args);
 }
 
 void throwArrayIndexOutOfBoundsException(JNIEnv *env, const char *format, ...) {
     va_list args;
     va_start(args, format);
-    jclass exceptionClass = env->FindClass("java/lang/ArrayIndexOutOfBoundsException");
-    throwException(env, &exceptionClass, format, args);
+    jclass exception_class = env->FindClass("java/lang/ArrayIndexOutOfBoundsException");
+    throwException(env, &exception_class, format, args);
     va_end(args);
 }
 
 void throwIOException(JNIEnv *env, const char *format, ...) {
     va_list args;
     va_start(args, format);
-    jclass exceptionClass = env->FindClass("java/io/IOException");
-    throwException(env, &exceptionClass, format, args);
+    jclass exception_class = env->FindClass("java/io/IOException");
+    throwException(env, &exception_class, format, args);
     va_end(args);
 }
 
 void throwFileNotFoundException(JNIEnv *env, const char *format, ...) {
     va_list args;
     va_start(args, format);
-    jclass exceptionClass = env->FindClass("java/io/FileNotFoundException");
-    throwException(env, &exceptionClass, format, args);
+    jclass exception_class = env->FindClass("java/io/FileNotFoundException");
+    throwException(env, &exception_class, format, args);
     va_end(args);
 }
 
 void throwArithmeticException(JNIEnv *env, const char *format, ...) {
     va_list args;
     va_start(args, format);
-    jclass exceptionClass = env->FindClass("java/lang/ArithmeticException");
-    throwException(env, &exceptionClass, format, args);
+    jclass exception_class = env->FindClass("java/lang/ArithmeticException");
+    throwException(env, &exception_class, format, args);
     va_end(args);
 }
 
 void throwUnsupportedOperationException(JNIEnv *env, const char *format, ...) {
     va_list args;
     va_start(args, format);
-    jclass exceptionClass = env->FindClass("java/lang/UnsupportedOperationException");
-    throwException(env, &exceptionClass, format, args);
+    jclass exception_class = env->FindClass("java/lang/UnsupportedOperationException");
+    throwException(env, &exception_class, format, args);
     va_end(args);
 }
 
 void throwIllegalStateException(JNIEnv *env, const char *format, ...) {
     va_list args;
     va_start(args, format);
-    jclass exceptionClass = env->FindClass("java/lang/IllegalStateException");
-    throwException(env, &exceptionClass, format, args);
+    jclass exception_class = env->FindClass("java/lang/IllegalStateException");
+    throwException(env, &exception_class, format, args);
     va_end(args);
 }
 
 void throwNoSuchElementException(JNIEnv *env, const char *format, ...) {
     va_list args;
     va_start(args, format);
-    jclass exceptionClass = env->FindClass("java/util/NoSuchElementException");
-    throwException(env, &exceptionClass, format, args);
+    jclass exception_class = env->FindClass("java/util/NoSuchElementException");
+    throwException(env, &exception_class, format, args);
     va_end(args);
 }
 
 void throwNumberFormatException(JNIEnv *env, const char *format, ...) {
     va_list args;
     va_start(args, format);
-    jclass exceptionClass = env->FindClass("java/lang/NumberFormatException");
-    throwException(env, &exceptionClass, format, args);
+    jclass exception_class = env->FindClass("java/lang/NumberFormatException");
+    throwException(env, &exception_class, format, args);
     va_end(args);
 }
 
 void throwRuntimeException(JNIEnv *env, const char *format, ...) {
     va_list args;
     va_start(args, format);
-    jclass exceptionClass = env->FindClass("java/lang/RuntimeException");
-    throwException(env, &exceptionClass, format, args);
+    jclass exception_class = env->FindClass("java/lang/RuntimeException");
+    throwException(env, &exception_class, format, args);
     va_end(args);
 }
 
