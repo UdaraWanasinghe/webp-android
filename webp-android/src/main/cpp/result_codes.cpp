270422f4a3a18fc2645eebf436b3a6e7fa3f30c6
Udara Wanasinghe <udara.developer@gmail.com>
2023-06-20 15:47:14 +0530

    update result codes

--- webp-android/src/main/cpp/result_codes.cpp
+++ webp-android/src/main/cpp/result_codes.cpp
@@ -3,21 +3,23 @@
 //
 
 #include <string>
-#include "include/error_codes.h"
+#include "include/result_codes.h"
 
-std::string parseErrorMessage(int error_code) {
-    switch (error_code) {
+std::string parseResultMessage(int result_code) {
+    switch (result_code) {
+        case RESULT_FILE_EXISTS:
+            return "Given file path exists";
         case ERROR_NULL_ENCODER:
             return "Encoder cannot be null";
         case ERROR_INVALID_ENCODER_INSTANCE:
-            return "Given Java object is not a valid encoder instance";
+            return "Given object is not a encoder instance";
         case ERROR_VERSION_MISMATCH:
             return "Version mismatch";
-        case ERROR_FAILED_TO_GET_BITMAP_INFO:
+        case ERROR_BITMAP_INFO_EXTRACT_FAILED:
             return "Failed to get bitmap info";
         case ERROR_INVALID_BITMAP_FORMAT:
             return "Bitmap is not formatted with RGBA_8888 format";
-        case ERROR_RESIZE_BITMAP_FAILED:
+        case ERROR_BITMAP_RESIZE_FAILED:
             return "Failed to resize bitmap";
         case ERROR_LOCK_BITMAP_PIXELS_FAILED:
             return "Failed to lock bitmap pixels";
@@ -32,23 +34,27 @@ std::string parseErrorMessage(int error_code) {
         case ERROR_ANIMATION_ASSEMBLE_FAILED:
             return "Failed to assemble the animation";
         case ERROR_INVALID_WEBP_CONFIG:
-            return "Invalid WebPConfig";
+            return "Invalid webp config";
         case ERROR_BITMAP_URI_DECODE_FAILED:
             return "Failed to decode bitmap uri";
         case ERROR_WRITE_TO_URI_FAILED:
             return "Failed to write to the given uri";
         case ERROR_WEBP_ENCODE_FAILED:
-            return "WebPEncode failed";
+            return "Webp encode failed";
         case ERROR_INVALID_DECODER_INSTANCE:
-            return "Given decoder is not a valid decoder instance";
+            return "Given decoder is not a decoder instance";
         case ERROR_READ_URI_FAILED:
             return "Failed to read from the given uri";
-        case ERROR_READ_WEBP_INFO_FAILED:
-            return "Failed to read WebP info";
+        case ERROR_WEBP_INFO_EXTRACT_FAILED:
+            return "Failed to read webp info";
         case ERROR_WEBP_DECODE_FAILED:
             return "Failed to decode webp data";
-        case ERROR_WRITE_BITMAP_TO_URI_FAILED:
+        case ERROR_BITMAP_WRITE_TO_URI_FAILED:
             return "Failed to write bitmap to the uri";
+        case ERROR_NULL_DECODER:
+            return "Decoder cannot be null";
+        case ERROR_JAVA_EXCEPTION:
+            return "Java exception occurred";
         default:
             return "Unknown error.";
     }
