68d4650f3ddfbcaa6b18a68ab684cf0ba5b02bda
Udara Wanasinghe <udara.developer@gmail.com>
2023-06-23 23:56:39 +0530

    add anim encoder doc comments

--- webp-android/src/main/java/com/aureusapps/android/webpandroid/encoder/WebPAnimEncoder.kt
+++ webp-android/src/main/java/com/aureusapps/android/webpandroid/encoder/WebPAnimEncoder.kt
@@ -1,9 +1,17 @@
 package com.aureusapps.android.webpandroid.encoder
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 
+/**
+ * Constructs a WebP animation encoder with the specified width, height, and options.
+ *
+ * @param width The width of the animation frames in pixels.
+ * @param height The height of the animation frames in pixels.
+ * @param options The optional encoding options for the WebP animation. Default value is null.
+ */
 class WebPAnimEncoder(
     width: Int,
     height: Int,
@@ -27,10 +35,24 @@ class WebPAnimEncoder(
         }
     }
 
+    /**
+     * Adds a progress listener to the WebP animation encoder.
+     *
+     * @param listener The progress listener to be added.
+     *
+     * @return true if the listener was successfully added, false otherwise.
+     */
     fun addProgressListener(listener: WebPAnimEncoderProgressListener): Boolean {
         return progressListeners.add(listener)
     }
 
+    /**
+     * Removes a progress listener from the WebP animation encoder.
+     *
+     * @param listener The progress listener to be removed.
+     *
+     * @return true if the listener was successfully removed, false otherwise.
+     */
     fun removeProgressListener(listener: WebPAnimEncoderProgressListener): Boolean {
         return progressListeners.remove(listener)
     }
@@ -51,19 +73,53 @@ class WebPAnimEncoder(
         options: WebPAnimEncoderOptions?
     ): Long
 
+    /**
+     * Configures the WebP encoder with the specified configuration.
+     *
+     * @param config The WebP configuration to be applied.
+     * @param preset The optional preset to be used for configuring the encoder. Default value is null.
+     */
     external fun configure(
         config: WebPConfig,
         preset: WebPPreset? = null
     )
 
+    /**
+     * Adds a frame to the WebP animation with the specified timestamp and source bitmap.
+     *
+     * @param timestamp The timestamp of the frame.
+     * @param srcBitmap The source bitmap to be added as a frame.
+     */
     external fun addFrame(timestamp: Long, srcBitmap: Bitmap)
 
+    /**
+     * Adds a frame to the WebP animation with the specified timestamp and source Uri.
+     *
+     * @param context The Android context.
+     * @param timestamp The timestamp of the frame.
+     * @param srcUri The source Uri pointing to the image file to be added as a frame.
+     * This could be an Android content provider Uri, file Uri, Android resource Uri or a http Uri.
+     */
     external fun addFrame(context: Context, timestamp: Long, srcUri: Uri)
 
+    /**
+     * Assembles the WebP animation and saves it to the specified destination Uri.
+     *
+     * @param context The Android context.
+     * @param timestamp The end timestamp of the animation.
+     * @param dstUri The destination Uri where the WebP animation will be saved.
+     * This could be a content provider Uri returned by [Intent.ACTION_CREATE_DOCUMENT] or a file Uri.
+     */
     external fun assemble(context: Context, timestamp: Long, dstUri: Uri)
 
+    /**
+     * Cancels the ongoing WebP animation encoding process.
+     */
     external fun cancel()
 
+    /**
+     * Releases any resources associated with the WebP encoder.
+     */
     external fun release()
 
 }
